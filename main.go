271dbffb18c6f93ac738ebe5138286072a66def3
SparkKnight <51189910+SparkKnight84@users.noreply.github.com>
2021-12-30 12:44:00 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,112 +4,112 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-  "encoding/json"
+ 	"encoding/json"
 )
 
 func main() {
 	readFile()
-  goTypeToJsonString()
-  jsonStringToGoType()
+	goTypeToJsonString()
+	jsonStringToGoType()
 }
 
 func readFile() {
-  jsonFile, err := os.Open("file.json")
+ 	jsonFile, err := os.Open("file.json")
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	defer jsonFile.Close()
 
-  byteValue, _ := ioutil.ReadAll(jsonFile)
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-  fmt.Printf("%v\n", string(byteValue))
-  // [
-  //   {
-  //     "title": "Title 1",
-  //     "url": "Url 1",
-  //     "channel": {
-  //       "name": "Channel Name 1",
-  //       "url": "Channel Url 1"
-  //     }
-  //   },
-  //   {
-  //     "title": "Title 2",
-  //     "url": "Url 2",
-  //     "channel": {
-  //     "name": "Channel Name 2",
-  //     "url": "Channel Url 2"
-  //     }
-  //   },
-  //   {
-  //   "title": "Title 3",
-  //   "url": "Url 3",
-  //   "channel": {
-  //     "name": "Channel Name 3",
-  //       "url": "Channel Url 3"
-  //     }
-  //   }
-  // ]
-  fmt.Printf("%T\n", string(byteValue))
-  // string
+	fmt.Printf("%v\n", string(byteValue))
+ 	// [
+  	//   {
+  	//     "title": "Title 1",
+  	//     "url": "Url 1",
+  	//     "channel": {
+  	//       "name": "Channel Name 1",
+  	//       "url": "Channel Url 1"
+  	//     }
+  	//   },
+  	//   {
+  	//     "title": "Title 2",
+  	//     "url": "Url 2",
+  	//     "channel": {
+  	//     "name": "Channel Name 2",
+  	//     "url": "Channel Url 2"
+  	//     }
+  	//   },
+  	//   {
+  	//   "title": "Title 3",
+  	//   "url": "Url 3",
+  	//   "channel": {
+  	//     "name": "Channel Name 3",
+  	//       "url": "Channel Url 3"
+  	//     }
+  	//   }
+  	// ]
+  	fmt.Printf("%T\n", string(byteValue))
+  	// string
 }
 
 func jsonStringToGoType() {
-  jsonString := []byte(`{
-    "title": "Title 1",
-    "url": "Url 1",
-    "channel": {
-      "name": "Channel Name 1",
-      "url": "Channel Url 1"
-    }
-  }`)
+  	jsonString := []byte(`{
+    		"title": "Title 1",
+    		"url": "Url 1",
+    		"channel": {
+      		"name": "Channel Name 1",
+      		"url": "Channel Url 1"
+    		}
+  	}`)
 
-  type channel struct {
-    Name string
-    URL string
-  }
+  	type channel struct {
+    		Name string
+    		URL string
+  	}
 
-  type myType struct {
-    Title string
-    URL string
-    Channel channel
-  }
+  	type myType struct {
+    		Title string
+    		URL string
+    		Channel channel
+  	}
 
-  var goType myType
+  	var goType myType
 
-  err := json.Unmarshal(jsonString, &goType)
-  if err != nil {
-    fmt.Println(err)
-  }
+  	err := json.Unmarshal(jsonString, &goType)
+  	if err != nil {
+    		fmt.Println(err)
+  	}
 
-  fmt.Printf("%+v\n", goType)
-  // {Title:Title 1 URL:Url 1 Channel:{Name:Channel Name 1 URL:Channel Url 1}}
-  fmt.Printf("%T\n", goType)
-  // main.myType
+  	fmt.Printf("%+v\n", goType)
+  	// {Title:Title 1 URL:Url 1 Channel:{Name:Channel Name 1 URL:Channel Url 1}}
+  	fmt.Printf("%T\n", goType)
+  	// main.myType
 }
 
 func goTypeToJsonString() {
   
-  type channel struct {
-    Name string
-    URL string
-  }
+  	type channel struct {
+    		Name string
+    		URL string
+  	}
 
-  type myType struct {
-    Title string
-    URL string
-    Channel channel
-  }
+  	type myType struct {
+    		Title string
+    		URL string
+    		Channel channel
+  	}
 
-  data := myType{"Title 1", "Url 1", channel{"Channel Name 1", "Channel Url 1"}}
+  	data := myType{"Title 1", "Url 1", channel{"Channel Name 1", "Channel Url 1"}}
 
-  jsonData, err := json.Marshal(data)
-  if err != nil {
-    fmt.Println(err)
-  }
+  	jsonData, err := json.Marshal(data)
+  	if err != nil {
+    		fmt.Println(err)
+  	}
 
-  fmt.Println(string(jsonData))
-  // {"Title":"Title 1","URL":"Url 1","Channel":{"Name":"Channel Name 1","URL":"Channel Url 1"}}
-  fmt.Printf("%T\n", string(jsonData))
-  // string
+  	fmt.Println(string(jsonData))
+  	// {"Title":"Title 1","URL":"Url 1","Channel":{"Name":"Channel Name 1","URL":"Channel Url 1"}}
+  	fmt.Printf("%T\n", string(jsonData))
+  	// string
 }
